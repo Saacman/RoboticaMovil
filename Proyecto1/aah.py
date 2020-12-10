f65fd2e39d7a78e578713bfd372c571ea9ae44f6
Isaac Sanchez <saacman27@gmail.com>
2020-12-09 22:54:04 -0600

    ordenar

--- Proyecto1/aah.py
+++ Proyecto1/aah.py
@@ -24,6 +24,7 @@ else:
 err, motorL = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx_leftMotor', vrep.simx_opmode_blocking)
 err, motorR = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx_rightMotor', vrep.simx_opmode_blocking)
 err, robot = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx', vrep.simx_opmode_blocking)
+err, goal = vrep.simxGetObjectHandle(clientID, 'Sphere', vrep.simx_opmode_blocking)
 
 # Assigning handles to the ultrasonic sensors
 usensor = []
@@ -39,7 +40,7 @@ for i in range(16):
 #<----------------------------------A* Algorithm---------------------------------------->
 
 # Load the grid
-occmap = GridMap.loadImg("rescue.png")
+occmap = GridMap.loadImg("map.png")
 grid = np.array(occmap.grid, copy=True)
 
 # Dilate the obs
@@ -47,11 +48,26 @@ bin_grid = np.int8(grid > 0.5)
 disk = selem.disk(4)
 obstacles = binary_dilation(bin_grid, disk)
 
-start = (20, 70)
-end = (90, 30)
+# Get the starting position from the robot
+err, pos0 = vrep.simxGetObjectPosition(clientID, robot, -1, vrep.simx_opmode_blocking)
+pos0 = np.array(pos0[:2])
+tpos0 = ((pos0 - occmap.ipos) / occmap.tsize).astype(int) + occmap.coffset
 
+# Get the position of the goal
+err, g_pos = vrep.simxGetObjectPosition(clientID, goal, -1, vrep.simx_opmode_blocking)
+g_pos = np.array(g_pos[:2])
+tg_pos = ((g_pos - occmap.ipos) / occmap.tsize).astype(int) + occmap.coffset
+
+# Set A* Values and get the path
+start = tuple(tpos0[::-1]) #(y,x)
+end = tuple(tg_pos[::-1])
 path = astar(obstacles, start, end, allow_diagonal_movement=True)
 path = np.array(path)
+
+# Plot the path
+grid[path[:,0], path[:,1]] = 0.3
+plt.imshow(np.flipud(grid))
+plt.show()
 
 # Transform the obtained path
 trans_path = np.empty_like(path).astype(float)
@@ -72,12 +88,18 @@ while step < len(pointsx)-1:
     step = step + 1 if errp < 0.2 else step
     # Get the motors speed
     errp, ulc, urc, pos, rot = pc.continuosControl(clientID, robot, (pointsx[step], pointsy[step]))
-
-    ul = ulc
-    ur = urc
-
-    errf = vrep.simxSetJointTargetVelocity(clientID, motorL, ul, vrep.simx_opmode_streaming)
-    errf = vrep.simxSetJointTargetVelocity(clientID, motorR, ur, vrep.simx_opmode_streaming)
+    """
+    # This block activates the braitenberg algorithm, in case the space is narrow
+    avoid, ulb, urb = pc.braitenberg(clientID, usensor)
+    if avoid:
+        ul = ulb
+        ur = urb
+    else:
+        ul = ulc
+        ur = urc
+    """
+    errf = vrep.simxSetJointTargetVelocity(clientID, motorL, ulc, vrep.simx_opmode_streaming)
+    errf = vrep.simxSetJointTargetVelocity(clientID, motorR, urc, vrep.simx_opmode_streaming)
 
 #The End
 vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
